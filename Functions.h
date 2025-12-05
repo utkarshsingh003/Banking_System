ad0a73b998613ea1025d9789d3aa9c843d260cbc
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:46:27 +0530

    Refactor account functions to use smart pointers
    Updated account management functions to use unique_ptr and shared_ptr for better memory management.

--- Functions.h
+++ Functions.h
@@ -6,24 +6,12 @@
 #include "Trust.h"
 #include <iostream>
 #include <vector>
+#include <memory>
 using namespace std;
 
 //For Savings
-void display(vector <Savings>& accounts);
-void deposit(vector <Savings>& accounts,double amount);
-void withdraw(vector <Savings>& accounts, double amount);
+void display(unique_ptr<vector<shared_ptr<Account>>>& accounts);
+void deposit(unique_ptr<vector<shared_ptr<Account>>>& accounts);
+void withdraw(unique_ptr<vector<shared_ptr<Account>>>& accounts);
+void history(unique_ptr<vector<shared_ptr<Account>>>& accounts);
 
-//For Current
-void display(vector <Current>& accounts);
-void deposit(vector <Current>& accounts,double amount);
-void withdraw(vector <Current>& accounts,double amount);
-
-//For Checking
-void display(vector <Checking>& accounts);
-void deposit(vector <Checking>& accounts,double amount);
-void withdraw(vector <Checking>& accounts,double amount);
-
-//For Trust
-void display(vector <Trust>& accounts);
-void deposit(vector <Trust>& accounts,double amount);
-void withdraw(vector <Trust>& accounts,double amount);
