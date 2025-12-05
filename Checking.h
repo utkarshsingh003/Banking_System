4eeeea2539826432db4c0452713f10eff0090c21
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:37:13 +0530

    Refactor Checking class by removing private members
    Removed friend functions and private member variables from Checking class.

--- Checking.h
+++ Checking.h
@@ -6,18 +6,10 @@ using namespace std;
 
 class Checking :public Account
 {
-	friend ostream& operator<<(ostream& os, const Checking& rhs);
-	friend istream& operator>>(istream& is, Checking& rhs);
-
-private:
-	string name;
-	int age;
-	string PAN;
-	double balance;
-	long int Acc_no;
 
 public:
 	Checking();
-	bool deposit(double amount);
-	bool withdraw(double amount);
+	Checking(string n, int a, string P, double b);
+	virtual bool deposit(double amount);
+	virtual bool withdraw(double amount);
 };
