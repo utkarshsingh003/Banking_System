27b15269c227f036711e05463bee68b35943adfe
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:40:19 +0530

    Refactor Current class for account management
    Called base class methods inside the methods of the inherited classes for efficiency ,  this allows reusability.

--- Current.cpp
+++ Current.cpp
@@ -3,111 +3,87 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
-ostream& operator<<(ostream& os, const Current& rhs)
-{
-	os << "[Account holders's name: " << rhs.name << " Age: " << rhs.age << " PAN no: " << rhs.PAN << " Balance: " << rhs.balance << "Account no: " << rhs.Acc_no << " Withdrawal Limit: " << rhs.limits << " ]";
-	return os;
-}
-istream& operator>>(istream& is, Current& rhs)
-{
-	char n{};
-	bool valid{ true };
-
-	cout << "Enter Your name: ";
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	getline(is, rhs.name);
-
-	cout << "\nEnter your age: ";
-	is >> rhs.age;
-	if (rhs.age < 18)
-	{
-		cout << "You cant open a current account!" << endl;
-		valid = false;
-        return is; 
-	}
-	cout << "\nDo you have a PAN card [Y/N] ";
-	is >> n;
-
-	if (n == 'Y' || n == 'y')
-	{
-		cout << "Enter your PAN number: ";
-		is >> rhs.PAN;
-	}
-
-	else
-	{
-		cout << "\nYou cant open a current account!" << endl;
-		valid = false;
-		return is;
-	}
-
-	cout << "\nDeposit atleast 1000$ to open account ";
-	is >> rhs.balance;
-
-	if (rhs.balance < 1000)
-	{
-		cout << "\nYou cant open current account!" << endl;
-		valid = false;
-		return is;
-	}
-	if (valid == true)
-		cout << "Account opened successfully!" << endl;
-
-	return is;
-}
 Current::Current()
-	:name{ "None" }, age{ 0 }, PAN{ "None" }, balance{ 0.0 }, limits{ 0 }
+	:Account(), limits{0} 
+{
+	srand(time(0));
+	Acc_no = rand() % 100000 + 1;
+}
+Current::Current(string n, int a, string P, double b)
+	:Account{ n,a,P,b }, limits{ 0 }
 {
 	srand(static_cast<unsigned int>(time(0)));
 	Acc_no = rand() % 100000 + 1;
 }
 bool Current::deposit(double amount)
 {
-	long int acc{};
-	cout << "Enter your account number: ";
-	cin >> acc;
-
-	if (acc != Acc_no) {
-		cout << "Sorry, account number doesn't match!" << endl;
+	ofstream out_file{ "Current.txt",ios::app };
+	if (!out_file)
+	{
+		cerr << "Can't open a file!" << endl;
 		return false;
 	}
-	else
-	{
-		if (amount <= 0)
-			return false;
+	ostringstream oss{};
 
-		else
-		{
-			balance += amount;
-			return true;
-		}
+	if (Account::withdraw(amount))
+	{
+		oss << "Name: " << name
+			<< "\nAge: " << age
+			<< "\nPAN: " << PAN
+			<< "\nDeposted: " << amount
+			<< "\nBalance: " << balance
+			<< "\nAccount no.: " << Acc_no << endl;
+
+		out_file << oss.str();
+
+		return true;
 	}
+	else
+		return false;
+	
 }
 
 bool Current::withdraw(double amount)
 {
-	long int acc{};
-	cout << "Enter your account number: ";
-	cin >> acc;
-
-	if (acc != Acc_no)
-		cout << "Sorry Account number doesn't match!" << endl;
-
-	else
+	ofstream out_file{ "Current.txt",ios::app };
+	if (!out_file)
 	{
-		while (limits < 10)
-		{
-			if (amount > balance)
-				return false;
-
-			else
-			{
-				balance -= amount;
-				limits += 1;
-				return true;
-			}
-		}
+		cerr << "Can't open a file!" << endl;
+		return false;
 	}
+	ostringstream oss{};
+
+	while(limits < 10)
+	{
+		if (Account::withdraw(amount))
+		{
+			limits++;
+
+			oss << "\nName: " << name
+				<< "\nAge: " << age
+				<< "\nPAN: " << PAN
+				<< "\nWithdrawal limit" << limits
+				<< "\nDeposted: " << amount
+				<< "\nBalance: " << balance
+				<< "\nAccount no.: " << Acc_no << endl;
+
+			out_file << oss.str();
+
+			return true;
+		}
+		else
+			return false;
+		
+	}
+}
+void Current::print(ostream& os) const
+{
+	os << "[Account holder's name: " << name << " Age: "
+		<< age << " PAN no: " << PAN << " Account no: "
+		<< Acc_no << " Balance: " << balance << " withdrawal limit: " << limits << " ]";
 }
