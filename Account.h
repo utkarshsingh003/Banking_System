92f3bf065cb5f6e5208d17b0ad9218f470b59487
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:33:17 +0530

    Refactor Account class to implement Printable interface
    In order to prevent other classes from creating an overloaded operator repeatedly, I created a printable abstract class. I have implemented protected members so that I don't have to declare them repeatedly, and I have created virtual methods to reuse the existing code.

--- Account.h
+++ Account.h
@@ -3,9 +3,26 @@
 #include <string>
 using namespace std;
 
-class Account
+class Printable
 {
-private:
+	friend ostream& operator<<(ostream& os, const Printable& rhs)
+	{
+		rhs.print(os);
+		return os;
+	}
+	friend istream& operator>>(istream& is, Printable& rhs)
+	{
+		rhs.input(is);
+		return is;
+	}
+public:
+	virtual void print(ostream& os) const = 0;
+	virtual void input(istream& is) = 0;
+};
+
+class Account :public Printable
+{
+protected:
 	string name;
 	int age;
 	string PAN;
@@ -15,7 +32,8 @@ private:
 public:
 	Account();
 	Account(string n, int a, string P, double b);
-	bool deposit(double amount);
-	bool withdraw(double amount);
-
+	virtual bool deposit(double amount);
+	virtual bool withdraw(double amount);
+	virtual void print(ostream& os) const override;
+	virtual void input(istream& is) override;
 };
