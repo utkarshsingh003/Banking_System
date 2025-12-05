580b2b6bb7a9c67665c49a11fa170b311aa70bac
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:36:33 +0530

    Refactor Checking class and add file logging
    To enhance the account opening process, refactor the checking class and include file logging for deposits and withdrawals.Additionally, the classes were constructed using the base class constructor.logged the deposits and withdrawals into the file using ostringstream and ofstream.

--- Checking.cpp
+++ Checking.cpp
@@ -3,118 +3,75 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-ostream& operator<<(ostream& os, const Checking& rhs)
-{
-	os << "[Account holder's name: " << rhs.name << " Age: " << rhs.age << " PAN no: " << rhs.PAN << " Account no: " << rhs.Acc_no << " Balance: " << rhs.balance << " ]";
-	return os;
-}
-istream& operator>>(istream& is, Checking& rhs)
-{
-	char n{};
-	bool valid{ true };
-
-	cout << "Enter Your name: ";
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	getline(is, rhs.name);
-
-
-	cout << "\nEnter your age: ";
-	is >> rhs.age;
-	if (rhs.age < 18)
-	{
-		cout << "You cant open a cheking account" << endl;
-		valid = false;
-		return is;
-	}
-
-	cout << "\nDo you have a PAN card [Y/N] ";
-	is >> n;
-
-	if (n == 'Y' || n == 'y')
-	{
-		cout << "Enter your PAN number: ";
-		is >> rhs.PAN;
-	}
-
-	else
-	{
-		cout << "\nYou cant open a checking account" << endl;
-		valid = false;
-		return is;
-	}
-
-	cout << "\nDeposit atleast 500$ to open an account" << endl;
-	is >> rhs.balance;
-
-	if (rhs.balance < 500 )
-	{
-		cout << "\nYou cant open checking account" << endl;
-		valid = false;
-		return is;
-	}
-	if (valid == true)
-		cout << "Account opened successfully!" << endl;
-
-	return is;
-}
-
-
 Checking::Checking()
-	:name{ "None" }, age{ 0 }, PAN{ "None" }, balance{ 0.0 }
+	:Account()
+{
+	srand(time(0));
+	Acc_no = rand() % 100000 + 1;
+}
+Checking::Checking(string n, int a, string P, double b)
+	:Account{ n,a,P,b }
 {
 	srand(static_cast<unsigned int>(time(0)));
 	Acc_no = rand() % 100000 + 1;
 }
 bool Checking::deposit(double amount)
 {
-	long int acc{};
-	cout << "Enter your account number: ";
-	cin >> acc;
-
-	if (acc != Acc_no)
-		cout << "Sorry Account number doesn't match!" << endl;
-
-	else
+	ofstream out_file{ "Checking.txt",ios::app };
+	if (!out_file)
 	{
-		if (amount <= 0)
-			return false;
-
-		else
-		{
-			balance += amount;
-			return true;
-		}
+		cerr << "Can't open a file!" << endl;
+		return false;
 	}
+	ostringstream oss{};
+
+	if (Account::deposit(amount))
+	{
+		oss << "Name: " << name
+			<< "\nAge: " << age
+			<< "\nPAN: " << PAN
+			<< "\nDeposted: " << amount
+			<< "\nBalance: " << balance
+			<< "\nAccount no.: " << Acc_no << endl;
+
+		out_file << oss.str();
+
+		return true;
+	}
+	else
+		return false;
 }
 
 bool Checking::withdraw(double amount)
 {
-	long int acc{};
-	cout << "Enter your account number: ";
-	cin >> acc;
-
-	if (acc != Acc_no)
-		cout << "Sorry Account number doesn't match!" << endl;
-
-	else
+	ofstream out_file{ "Checking.txt",ios::app };
+	if (!out_file)
 	{
-		if (amount > balance)
-			return false;
-
-		else
-		{
-			balance -= amount;
-			if (balance == 0)
-				return false;
-
-			else
-			{
-				balance -= 1.50;
-				return true;
-			}
-		}
+		cerr << "Can't open a file!" << endl;
+		return false;
 	}
+
+	ostringstream oss{};
+
+	amount -= 1.50;
+	if (Account::withdraw(amount))
+	{
+		oss << "\nName: " << name
+			<< "\nAge: " << age
+			<< "\nPAN: " << PAN
+			<< "\nWithdrawn: " << amount
+			<< "\nBalance: " << balance
+			<< "\nAccount no.: " << Acc_no << endl;
+
+		out_file << oss.str();
+
+		return true;
+	}
+	else
+		return false;
 }
 
