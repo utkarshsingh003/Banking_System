23553cdf10cf092020a13e9d5e5ea49207a85451
utkarshsingh003 <146013772+utkarshsingh003@users.noreply.github.com>
2025-12-05 18:34:14 +0530

    Enhance Account class with account number checks
    Added account number verification for deposit and withdrawal methods. Implemented input and print methods for account details.

--- Account.cpp
+++ Account.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 
 Account::Account()
-	:name{ "None" }, age{ 0 }, PAN{ "None" }, balance{ 0.0 }, Acc_no{ 000000 }
+	:name{ "None" }, age{ 0 }, PAN{ "None" }, balance{ 0.0 }
 {
-
+	srand(time(0));
+	Acc_no = rand() % 100000 + 1;
 }
 Account::Account(string n, int a, string P, double b)
 	:name{ n }, age{ a }, PAN{ P }, balance{ b }
@@ -17,23 +18,91 @@ Account::Account(string n, int a, string P, double b)
 }
 bool Account::deposit(double amount)
 {
-	if (amount <= 0)
-		return false;
+	long int acc{};
+	cout << "Enter Your account number: ";
+	cin >> acc;
+
+	if (acc != Acc_no)
+		cout << "Sorry Account number doesn't match!" << endl;
 
 	else
 	{
-		balance += amount;
-		return true;
+		if (amount <= 0)
+			return false;
+
+		else
+		{
+			balance += amount;
+			return true;
+		}
 	}
 }
 bool Account::withdraw(double amount)
 {
-	if (amount > balance)
-		return false;
+	long int acc{};
+	cout << "Enter Your account number: ";
+	cin >> acc;
+
+	if (acc != Acc_no)
+		cout << "Sorry Account number doesn't match!" << endl;
 
 	else
 	{
-		balance -= amount;
-		return true;
+		if (amount > balance)
+			return false;
+
+		else
+		{
+			balance -= amount;
+			return true;
+		}
 	}
+}
+void Account::print(ostream& os) const
+{
+	os << "[Account holder's name: " << name << " Age: " << age << " PAN no: " << PAN << " Account no: " << Acc_no << " Balance: " << balance << " ]";
+}
+void Account::input(istream& is)
+{
+	char n{};
+	bool valid{ true };
+
+	cout << "Enter Your name: ";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	getline(is, name);
+
+	cout << "\nEnter your age: ";
+	is >> age;
+	if (age < 18)
+	{
+		cout << "You cant open a trust account" << endl;
+		valid = false;
+	}
+
+
+	cout << "\nDo you have a PAN card [Y/N] ";
+	is >> n;
+
+	if (n == 'Y' || n == 'y')
+	{
+		cout << "Enter your PAN number:";
+		is >> PAN;
+	}
+
+	else
+	{
+		cout << "\nYou cant open a trust account" << endl;
+		valid = false;
+	}
+
+	cout << "\nDeposit atleast 500$ to open account ";
+	is >> balance;
+
+	if (balance < 500)
+	{
+		cout << "\nYou cant open a trust account" << endl;
+		valid = false;
+	}
+	if (valid == true)
+		cout << "Account opened successfully!" << endl;
 }
